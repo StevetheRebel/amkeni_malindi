686e7b6002f317bc6881198eaad9d83f3b7e2511
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2025-01-01 12:39:52 +0300

    added logos

--- src/assets/Donor Organization/index.js
+++ src/assets/Donor Organization/index.js
@@ -5,6 +5,8 @@ import donorOrg4 from "./KenyaSeal.png";
 import donorOrg5 from "./KRC.png";
 import donorOrg6 from "./PositiveVibes.png";
 import donorOrg7 from "./UHAI.png";
+import donorOrg8 from "./NASCOP.png";
+import donorOrg9 from "./KEMRI.png";
 
 const donorLogos = {
   donorOrg1,
@@ -14,6 +16,8 @@ const donorLogos = {
   donorOrg5,
   donorOrg6,
   donorOrg7,
+  donorOrg8,
+  donorOrg9,
 };
 
 export default donorLogos;
