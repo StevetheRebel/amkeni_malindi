e7ec81c4b1bd0db51661c144ff744b08e9606520
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2025-01-03 11:13:24 +0300

    just committing

--- src/Components/pages/Program.jsx
+++ src/Components/pages/Program.jsx
@@ -5,9 +5,9 @@ function Program() {
   return (
     <div className="relative h-auto top-[154px] xs:top-[160px] sm:top-36 ">
     <section className="flex flex-col gap-4 px-4 md:px-14 lg:px-[6%]">
-      <h1 className="h1-text capitalize text-center">our programs</h1>
+      <h1 className="h1-text capitalize text-center text-secondary">our programs</h1>
       <div>
-        <h2 className="h2-text text-secondary">Healthcare Promotion & Service Delivery</h2>
+        <h2 className="h2-text text-secondary/70">Healthcare Promotion & Service Delivery</h2>
         
       </div>
     </section>
@@ -15,5 +15,7 @@ function Program() {
     </div>
   );
 }
+
+
 
 export default Program;
