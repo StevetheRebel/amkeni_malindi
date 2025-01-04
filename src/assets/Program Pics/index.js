4358fa0fa0f6d3f7f09f6b6aeba40e8cb6282718
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2025-01-04 20:38:06 +0300

    additions

---
+++ src/assets/Program Pics/index.js
@@ -0,0 +1,15 @@
+import program1 from "./Patient-Doc.jpg"
+import program2 from "./STI.jpg"
+import program3 from "./Commodity.jpg"
+import program4 from "./PrEP and PEP.jpg"
+import program5 from "ARV.jpg"
+
+const programImages = {
+    program1,
+    program2,
+    program3,
+    program4,
+    program5,
+}
+
+export default programImages;
