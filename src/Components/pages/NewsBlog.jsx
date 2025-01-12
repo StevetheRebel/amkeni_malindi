f98561fd2b8b206a3e13a08143c4c091a9bc2e6a
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2025-01-12 11:52:27 +0300

    added the news section

--- src/Components/pages/NewsBlog.jsx
+++ src/Components/pages/NewsBlog.jsx
@@ -1,12 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../Footer/Footer";
+import portfolioData from "./../../portfolio.json";
 
 function NewsBlog() {
   return (
-    <div className="relative h-auto top-[154px] xs:top-[160px] sm:top-36 ">
+    <div className="relative select-none h-auto top-[154px] xs:top-[160px] sm:top-36 ">
+      <section className="px-4 lg:px-[6%]">
+        <h1 className="h2-text text-secondary text-center mb-2 md:mb-4">
+          Annual Milestones and Achievements
+        </h1>
+        <EventTimeline />
+      </section>
+      <section className="px-4 lg:px-[6%]">
+        <h1 className="h2-text text-secondary text-center my-2">Blog Space</h1>
+      </section>
       <Footer />
     </div>
   );
 }
+
+// Template for the Annual Milestones and Achievements
+const EventTimeline = () => {
+  const sortedData = [...portfolioData.portfolio].sort((a, b) => {
+    const getYear = (year) => {
+      return typeof year === "string" ? parseInt(year.split(" ")[0], 10) : year;
+    };
+
+    return getYear(b.year) - getYear(a.year);
+  });
+
+  const [selectedYear, setSelectedYear] = useState(sortedData[0].year);
+
+  return (
+    <>
+      <div className="flex flex-col w-full gap-4 sm:flex-row sm:gap-6">
+        {/* Render Button for Each Year */}
+        <div className="flex gap-2 flex-wrap justify-center py-2 sm:flex-col sm:gap-5 xl:gap-6 ">
+          {sortedData.map((item) => (
+            <button
+              key={item.year}
+              onClick={() => setSelectedYear(item.year)}
+              className={`px-2 py-1 body-text rounded-2xl text-nowrap ${
+                selectedYear === item.year ? "bg-primary" : "bg-primary/50"
+              } xs:px-4 xs:py-2`}
+            >
+              Year {item.year}
+            </button>
+          ))}
+        </div>
+
+        {/* Display events for the selected year */}
+        <div className="w-full">
+          <h2 className="h2-text text-secondary/70 text-center">
+            Events {selectedYear}
+          </h2>
+          <ul className="body-text list-disc list-inside flex flex-col gap-2 border-2 rounded-md p-2 md:gap-4 md:py-4 md:rounded-3xl ">
+            {sortedData
+              .find((item) => item.year === selectedYear)
+              .events.map((event, index) => (
+                <li key={index} className="bg-light p-1 rounded-md">
+                  {event}
+                </li>
+              ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+// Template for the Blog posts
 
 export default NewsBlog;
