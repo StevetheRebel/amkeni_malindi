25cf59e0bc812076590d3f5d9f3e290453796220
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2025-01-17 17:33:24 +0300

    added a blog route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navigation from "./../src/Components/Navigation/Navigation";
 import { Routes, Route } from "react-router-dom";
 import Home from "./../src/Components/pages/Home";
 import ScrollToTop from "./Components/ScrollToTop";
+import "./index.css";
 
 const About = lazy(() => import("./../src/Components/pages/About"));
 const Program = lazy(() => import("./../src/Components/pages/Program"));
@@ -11,6 +12,7 @@ const Opportunities = lazy(() =>
 );
 const NewsBlog = lazy(() => import("./../src/Components/pages/NewsBlog"));
 const Reachout = lazy(() => import("./../src/Components/pages/Reachout"));
+const BlogPost = lazy(() => import("./Components/pages/BlogPost"));
 
 function App() {
   return (
@@ -24,6 +26,7 @@ function App() {
           <Route path="/opportunities-services" element={<Opportunities />} />
           <Route path="/news-blog" element={<NewsBlog />} />
           <Route path="/reachout" element={<Reachout />} />
+          <Route path="/blog/:postId" element={<BlogPost />} />
         </Routes>
       </ScrollToTop>
     </>
