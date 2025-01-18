862853fb211f2fc085f007f6c95f977b6910393c
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2025-01-18 17:09:09 +0300

    additions

---
+++ src/assets/Pillar Pictures/index.js
@@ -0,0 +1,8 @@
+import AdvocacyImg from "./Advocacy.jpg"
+import HealthPromotionImg from "./HealthPromotion.jpg"
+import SustainableLivelihoodImg from './SustainableLivelihood.jpg'
+import SystemStrengtheningImg from './System Strengthening.png'
+
+export {
+    AdvocacyImg, HealthPromotionImg, SustainableLivelihoodImg, SystemStrengtheningImg
+}
