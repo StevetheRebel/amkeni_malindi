54c9fd39f846e74645d9b2459f14f47dc40729f6
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2025-01-25 11:09:25 +0300

    changes

--- src/assets/Pillar Pictures/index.js
+++ src/assets/Pillar Pictures/index.js
@@ -1,8 +1,11 @@
-import AdvocacyImg from "./Advocacy.jpg"
-import HealthPromotionImg from "./HealthPromotion.jpg"
-import SustainableLivelihoodImg from './SustainableLivelihood.jpg'
-import SystemStrengtheningImg from './System-Strengthening.jpg'
+import AdvocacyImg from "./Advocacy.jpg";
+import HealthPromotionImg from "./HealthPromotion.jpg";
+import SustainableLivelihoodImg from "./SustainableLivelihood.png";
+import SystemStrengtheningImg from "./System-Strengthening.png";
 
 export {
-    AdvocacyImg, HealthPromotionImg, SustainableLivelihoodImg, SystemStrengtheningImg
-}
+  AdvocacyImg,
+  HealthPromotionImg,
+  SustainableLivelihoodImg,
+  SystemStrengtheningImg,
+};
