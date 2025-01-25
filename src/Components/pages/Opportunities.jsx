40e1078d35afccd8d7521079cc7cd110489c9fc8
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2025-01-25 13:36:07 +0300

    redid the layout

--- src/Components/pages/Opportunities.jsx
+++ src/Components/pages/Opportunities.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Footer from "../Footer/Footer";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import RainbowLego from "./../../assets/OpportunitiesPhoto.jpg"
 
 function Opportunities() {
   return (
@@ -8,10 +10,18 @@ function Opportunities() {
         <h1 className="h1-text text-secondary text-center mb-2 md:mb-4 xl:mb-6 ">
           Opportunities
         </h1>
-        <div>
-          
+        <div className="relative flex flex-col gap-4 py-4 w-full md:py-0">
+          <div className="rounded-3xl w-full h-[40dvh] md:h-[50vh] ">
+            <LazyLoadImage src={RainbowLego} alt="queer lego background" className="w-full grayscale h-full object-cover rounded-2xl md:rounded-3xl hover:grayscale-0 transition-all duration-300 " />
+          </div>
+          <div className="bg-white/40 p-2 backdrop-blur-sm rounded-2xl md:bg-white md:w-[80%] md:absolute md:top-[90%] md:right-10 md:px-4 lg:px-8 ">
+            <h5 className="h5-text text-secondary text-pretty">
+              At present, there's no available opportunities. We encourage you
+              to check back regularly and stay connected through our social
+              media platforms for timely updates.
+            </h5>
+          </div>
         </div>
-        
       </section>
       <Footer />
     </div>
