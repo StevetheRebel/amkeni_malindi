bafb70ada71fd1c05ee13cf8e899e29ac794559c
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2025-01-25 12:58:39 +0300

    changes

--- src/assets/Sustainable Livelihoods/index.js
+++ src/assets/Sustainable Livelihoods/index.js
@@ -1,7 +1,7 @@
 import IncomeGen from "./IncomegeneratingProjects.webp"
 import SkillsTraining from "./SkillstrainingPrograms.webp"
 import ScholarshipOpp from "./ScholarshipOpportunities.webp"
-import ExchangeProg from "./ExchangePrograms.webp"
+import ExchangeProg from "./Exchangeprograms.webp"
 import LeadershipProg from "./Leadershipdevelopment.webp"
 import SelfImprovInit from "./SelfImprovementInitiatives.webp"
 
