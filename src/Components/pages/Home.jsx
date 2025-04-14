89387d32c24c4753e17f483ed2daefa11598f14e
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2025-04-14 09:53:14 +0300

    design shift

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination, Autoplay } from "swiper/modules";
+import { Pagination, Autoplay, EffectFade } from "swiper/modules";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChevronLeft,
@@ -24,13 +24,20 @@ import { HashLink } from "react-router-hash-link";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Link } from "react-router-dom";
 import he from "he";
-import pic1 from "./../../assets/Office Pics/Office1.webp"
+import pic1 from "./../../assets/Office Pics/Office1.webp";
 import ScrollAnimation from "react-animate-on-scroll";
-import "animate.css/animate.compat.css"
+import "animate.css/animate.compat.css";
+import "swiper/css/effect-fade";
+import ALogo from "./../../assets/Amkeni A.webp";
+import Typewriter from "../Typewriter";
 
 function Home() {
   const [posts, setPosts] = useState([]);
   const sortedPosts = [...posts];
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [typingComplete, setTypingComplete] = useState(false);
+  const swiperRef = useRef(null);
+  const secondSwiperRef = useRef(null);
 
   const [postLimit, setPostLimit] = useState(() => {
     if (window.innerWidth >= 1024) return 4;
@@ -66,8 +73,6 @@ function Home() {
   }, []);
 
   const officeImages = Object.values(images);
-  const firstSwiperRef = useRef(null);
-  const secondSwiperRef = useRef(null);
 
   const imagesMap = {
     "HealthPromotion.jpg": HealthPromotionImg,
@@ -83,96 +88,160 @@ function Home() {
     "/program-services#systems-strengthening",
   ];
 
+  const handleSlideChange = (swiper) => {
+    setActiveIndex(swiper.realIndex);
+    setTypingComplete(false);
+  };
+
   return (
     <div className="relative h-auto select-none scroll-smooth ">
       {/* Hero Section */}
-      <section className="relative min-h-dvh overflow-hidden">
-        <div className="fixed inset-0 bg-image9 bg-cover bg-center brightness-90 -z-10"></div>
-        <div className="pt-36 flex flex-col gap-4 items-center sm:gap-6 lg:px-[6%] lg:flex-row lg:gap-4 lg:pb-10 2xl:gap-20 ">
-                  {/* Carousel */}
-        <div className="relative w-[90%] h-[60vh] min-h-[320px] xs:min-h-[500px] shadow-custom mt-4 rounded-2xl lg:w-2/3 xl:min-h-[550px] group">
+      <section className="relative min-h-dvh overflow-hidden flex items-center justify-center pt-40 lg:px-[6%] ">
+        {/* Background Images carousel */}
+        <div className="fixed inset-0 -z-10">
           <Swiper
-            modules={[Pagination, Autoplay]}
-            pagination={{ clickable: true, dynamicBullets: true }}
-            autoplay={{ delay: 10000 }}
-            spaceBetween={30}
-            slidesPerView={1}
-            loop
-            speed={500}
-            className="h-full rounded-2xl"
-            onSwiper={(swiper) => (firstSwiperRef.current = swiper)}
+            modules={[Autoplay, EffectFade]}
+            effect="fade"
+            autoplay={{ delay: 10000, disableOnInteraction: false }}
+            loop={true}
+            speed={2000}
+            className="h-full w-full"
           >
             {officeImages.map((image, index) => (
-              <SwiperSlide key={index} className="h-full object-cover">
-                <img
-                  src={image}
-                  alt={`Office ${index + 1}`}
-                  className="w-full h-full object-cover rounded-2xl brightness-60"
+              <SwiperSlide key={index}>
+                <div
+                  className="w-full h-full"
+                  style={{
+                    backgroundImage: `url(${image})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                  }}
                 />
               </SwiperSlide>
             ))}
           </Swiper>
-          {/* Navigation Buttons */}
-          <div className="absolute inset-0 z-10 flex items-center justify-between px-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-            <button
-              onClick={() => firstSwiperRef.current?.slidePrev()}
-              className="bg-black/50 text-white p-3 rounded-full hover:bg-black focus:outline-none"
-            >
-              <FontAwesomeIcon icon={faChevronLeft} />
-            </button>
-            <button
-              onClick={() => firstSwiperRef.current?.slideNext()}
-              className="bg-black/50 text-white p-3 rounded-full hover:bg-black focus:outline-none"
-            >
-              <FontAwesomeIcon icon={faChevronRight} />
-            </button>
-          </div>
         </div>
+        <div className="fixed inset-0 bg-black/40 -z-10"></div>
+        {/* Background Images carousel */}
 
-        {/* Separator */}
-        <div className="border-2 w-[90%] border-white/80 lg:w-0 lg:h-[60vh] lg:min-h-[500px] xl:min-h-[550px] sm:mt-4"></div>
-
-        {/* Blog Posts */}
-        <div className="flex flex-col w-[90%] bg-muted/30 backdrop-blur-md rounded-3xl px-4 pb-4 mb-4 lg:h-[60vh] lg:min-h-[500px] lg:mt-4 lg:mb-0 lg:pb-0 xl:min-h-[550px] ">
-          <h4 className="h4-text text-white text-center w-full ">
-            Latest Posts...
-          </h4>
-
-          {/* Blog Spot */}
-          <div className="flex flex-col gap-4 h-full sm:flex-row lg:flex-col lg:gap-0 lg:py-4 lg:px-1 lg:justify-between xl:px-2 xl:py-4 2xl:px-4">
-            {sortedPosts.slice(0, postLimit).map((post, index) => {
-              const firstAttachmentKey = Object.keys(post.attachments)[0];
-              const imageUrl = post.attachments[firstAttachmentKey]?.URL || pic1;
-
-              return (
-                <div
-                key={index}
-                className={`relative overflow-hidden group w-full rounded-xl h-[88px] xs:h-[112px] md:h-[110px] lg:h-[100px] xl:h-[108px] 2xl:h-[115px] ${
-                  post.hiddenClass || ""
-                }`}
-              >
-                <LazyLoadImage
-                  src={imageUrl}
-                  alt={he.decode(post.title)}
-                  className="group-hover:grayscale group-hover:brightness-50 "
-                />
-                <div className="body-text flex flex-col justify-around h-full w-full font-bold text-white p-2 absolute top-[100%] group-hover:animate-slideUp lg:py-2">
-                  <h4 className="body-text">
-                    {truncateByWords(he.decode(post.title), 4)}
-                  </h4>
-                  <Link
-                    to={`/blog/${post.ID}`}
-                    className="button-type button-text self-end bg-primary/70 hover:bg-primary hover:text-black"
-                  >
-                    Read more
-                  </Link>
+        {/* Landing container */}
+        <div className="w-full grid grid-cols-1 gap-y-8 gap-x-12 place-items-center lg:grid-cols-5 xl:grid-cols-6  ">
+          {/* Carousel */}
+          <div className="relative w-[90%] h-[300px] grid-span-1 lg:col-span-3 xl:col-span-4 rounded-2xl overflow-hidden lg:w-full lg:h-auto lg:self-start xl:self-center ">
+            <Swiper
+              ref={swiperRef}
+              modules={[Autoplay]}
+              autoplay={{
+                delay: 15000,
+                disableOnInteraction: false,
+                waitForTransition: true,
+              }}
+              loop={true}
+              spaceBetween={30}
+              speed={1000}
+              allowTouchMove={false}
+              noSwiping={true}
+              onSlideChange={handleSlideChange}
+              className="h-full w-full"
+            >
+              {/* Logo */}
+              <SwiperSlide>
+                <div className="absolute h-[56px] w-full bg-black top-0 left-0 animate__animated animate__slideOutLeft -z-10 animate__slow animate__delay-1s md:h-[65px] lg:h-[76px] xl:h-[88px] 2xl:h-[113px]"></div>
+                <div>
+                  <h1 className=" h1-text px-2 bg-white tracking-tight xs:tracking-widest w-fit s:tracking-tight md:tracking-widest lg:tracking-wide 2xl:tracking-widest ">
+                    Welcome to
+                    <Typewriter text="..." delay={300} />
+                  </h1>
                 </div>
-              </div>
-              )
-})}
-          </div>
-        </div>
+                <div className="mt-6 flex items-end bg-white/20 backdrop-blur-sm rounded-b-2xl xs:justify-center">
+                  <img
+                    src={ALogo}
+                    alt=""
+                    className="w-32 animate__animated animate_fadeIn s:w-36 md:w-56 lg:w-44 xl:w-60 "
+                  />
+                  <h1 className="mb-3 text-5xl font-heading tracking-wider xs:text-6xl s:text-7xl md:text-9xl lg:text-8xl 2xl:text-[10rem] 2xl:mb-3 ">
+                    <Typewriter text="MKENI" delay={1000} />
+                  </h1>
+                </div>
+              </SwiperSlide>
 
+              {/* Vision */}
+              <SwiperSlide>
+                <div className="absolute h-[56px] w-full bg-black top-0 left-0 animate__animated animate__fadeIn animate__slideOutLeft -z-10 animate__slow animate__delay-1s md:h-[65px] lg:h-[76px] xl:h-[88px] 2xl:h-[113px] "></div>
+                <div>
+                  <h1 className=" h1-text px-2 bg-white tracking-tight xs:tracking-widest w-fit s:tracking-tight md:tracking-widest lg:tracking-wide 2xl:tracking-widest ">
+                    Our Vision
+                    <Typewriter text="..." delay={300} />
+                  </h1>
+                  <p className="py-2 font-subheading font-bold text-3xl mt-6 w-full text-center h-fit bg-white/20 backdrop-blur-sm rounded-b-2xl xs:text-4xl xs:py-4 xs:px-2 s:px-6 md:text-6xl lg:text-5xl 2xl:text-7xl 2xl:tracking-widest 2xl:py-8 2xl:px-16 ">
+                    <Typewriter
+                      text="An Empowered, Just and Inclusive Society."
+                      delay={100}
+                    />
+                  </p>
+                </div>
+              </SwiperSlide>
+
+              {/* Mission */}
+              <SwiperSlide>
+                <div className="absolute h-[56px] w-full bg-black top-0 left-0 animate__animated animate__fadeIn animate__slideOutLeft -z-10 animate__slow animate__delay-1s md:h-[65px] lg:h-[76px] xl:h-[88px] 2xl:h-[113px] "></div>
+                <div>
+                  <h1 className=" h1-text px-2 bg-white tracking-tight xs:tracking-widest w-fit s:tracking-tight md:tracking-widest lg:tracking-wide 2xl:tracking-widest ">
+                    Our Mission
+                    <Typewriter text="..." delay={300} />
+                  </h1>
+                  <p className="mt-6 py-2 font-subheading font-bold w-full text-center px-1 h-fit bg-white/30 backdrop-blur-sm rounded-b-2xl xs:px-4 xs:text-base s:text-lg s:py-4 md:text-2xl md:px-6 2xl:text-4xl 2xl:tracking-wider ">
+                    <Typewriter
+                      text="To improve the quality of life of sexual and gender diverse communities through an integrated approach to health service provision, social and economic empowerment, knowledge genaration and strategic advocacy."
+                      delay={30}
+                    />
+                  </p>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
+
+          {/* Blog posts */}
+          <div className="flex flex-col w-[90%] bg-muted/30 backdrop-blur-md rounded-3xl px-4 pb-4 mb-4 grid-span-1 lg:col-span-2 lg:h-[60vh] lg:min-h-[500px] lg:self-start lg:mb-0 lg:pb-0 xl:col-span-2 xl:min-h-[550px] 2xl:h-fit ">
+            <h4 className="h4-text text-white text-center w-full ">
+              Latest Posts...
+            </h4>
+
+            {/* Blog Spot */}
+            <div className="flex flex-col gap-4 h-full sm:flex-row lg:flex-col lg:gap-0 lg:py-4 lg:px-1 lg:justify-between xl:px-2 xl:py-4 2xl:px-4 2xl:gap-4">
+              {sortedPosts.slice(0, postLimit).map((post, index) => {
+                const firstAttachmentKey = Object.keys(post.attachments)[0];
+                const imageUrl =
+                  post.attachments[firstAttachmentKey]?.URL || pic1;
+
+                return (
+                  <div
+                    key={index}
+                    className={`relative overflow-hidden group w-full rounded-xl h-[88px] xs:h-[112px] md:h-[110px] lg:h-[100px] xl:h-[108px] 2xl:h-[145px] ${
+                      post.hiddenClass || ""
+                    }`}
+                  >
+                    <LazyLoadImage
+                      src={imageUrl}
+                      alt={he.decode(post.title)}
+                      className="group-hover:grayscale group-hover:brightness-50 "
+                    />
+                    <div className="body-text flex flex-col justify-around h-full w-full font-bold text-white p-2 absolute top-[100%] group-hover:animate-slideUp lg:py-2">
+                      <h4 className="body-text">
+                        {truncateByWords(he.decode(post.title), 4)}
+                      </h4>
+                      <Link
+                        to={`/blog/${post.ID}`}
+                        className="button-type button-text self-end bg-primary/70 hover:bg-primary hover:text-black"
+                      >
+                        Read more
+                      </Link>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </section>
 
@@ -180,9 +249,9 @@ function Home() {
       <section className="pt-6 pb-12 bg-light">
         {/* title */}
         <ScrollAnimation animateIn="flipInY" animateOut="flipOutY">
-        <h1 className="font-heading capitalize h1-text text-center text-secondary">
-          what we do
-        </h1>
+          <h1 className="font-heading capitalize h1-text text-center text-secondary">
+            what we do
+          </h1>
         </ScrollAnimation>
 
         {/* Carousel Container */}
