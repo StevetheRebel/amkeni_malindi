56a6a039e54a826af3086aff9b66ff85b4f818d5
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2025-05-18 11:24:15 +0300

    styling changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,29 +27,14 @@ export default {
           'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4)) ,url("/image2.jpg")',
         image3:
           'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4)) ,url("/image3.jpg")',
-        image4:
-          'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4)), url("/image4.jpg")',
-        image5: 'url("/image5.jpg")',
+        image5:
+          'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4)), url("/image5.jpg")',
         image6:
           'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4)), url("/image6.jpg")',
-        image7: 'url("/image7.jpg")',
-        image8: 'url("/image8.jpg")',
-        image9:
-          'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4)), url("/image9.jpg")',
-        image10:
-          'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4)), url("/image10.jpg")',
         image11:
           'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4)), url("/image11.jpg")',
         image12:
           'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4)) ,url("/image12.jpg")',
-        image13: 'url("/image13.jpg")',
-        image14: 'url("/image14.jpg")',
-        image15: 'url("/image15.jpg")',
-        image16: 'url("/image16.jpg")',
-        image17: 'url("/image17.jpg")',
-        image18: 'url("/image18.jpg")',
-        image19: 'url("/image19.jpg")',
-        image20: 'url("/image20.jpg")',
       },
       screens: {
         xs: "360px",
@@ -103,19 +88,20 @@ export default {
       listStyleType: {
         "lower-alpha": "lower-alpha",
         "lower-roman": "lower-roman",
-        "circle": "circle"
+        circle: "circle",
       },
       dropShadow: {
         "white-custom": "10px 4px 8px rgba(255, 255, 255, 0.7)",
       },
       boxShadow: {
-        "custom": "0px 0px 20px rgba(255, 255, 255, 0.7)",
+        custom: "0px 0px 20px rgba(255, 255, 255, 0.7)",
         "custom-shadow":
           "inset 19px 19px 16px #959f9d, inset -19px -19px 16px #ffffff",
         "custom-chat": "0px 3px 25px 0px rgba(0, 145, 255, 0.64);",
         "neomorph-shadow": "35px 35px 70px #818a88, -35px -35px 70px #ffffff",
         "neomorph-soft": "7px 7px 14px #818a88, -7px -7px 14px #ffffff",
-        "neomorph-other": "inset 20px 20px 60px #bdcac8, inset -20px -20px 60px #ffffff",
+        "neomorph-other":
+          "inset 20px 20px 60px #bdcac8, inset -20px -20px 60px #ffffff",
       },
     },
   },
