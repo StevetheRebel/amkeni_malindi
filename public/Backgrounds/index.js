309ca33ffa9021c57da1bc8e0cfbceff4d7e7b51
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2025-05-18 11:23:55 +0300

    backgrounds

---
+++ public/Backgrounds/index.js
@@ -0,0 +1,62 @@
+// About Us Images
+import AboutUsBig1 from "./AboutUs-big-1.webp";
+import AboutUsBig2 from "./AboutUs-big-2.webp";
+import AboutUsBig3 from "./AboutUs-big-3.webp";
+import AboutUsTab1 from "./AboutUs-tab-1.webp";
+import AboutUsTab2 from "./AboutUs-tab-2.webp";
+import AboutUsTab3 from "./AboutUs-tab-3.webp";
+import AboutUsTab4 from "./AboutUs-tab-4.webp";
+import AboutUsTab5 from "./AboutUs-tab-5.webp";
+import AboutUsTab6 from "./AboutUs-tab-6.webp";
+import AboutUsMobi1 from "./AboutUs-mobi-1.webp";
+import AboutUsMobi2 from "./AboutUs-mobi-2.webp";
+import AboutUsMobi3 from "./AboutUs-mobi-3.webp";
+import ProgramBig1 from "./Program-big-1.webp";
+import ProgramBig2 from "./Program-big-2.webp";
+import ProgramBig3 from "./Program-big-3.webp";
+import ProgramTab1 from "./Program-tab-1.webp";
+import ProgramTab2 from "./Program-tab-2.webp";
+import ProgramMobi1 from "./Program-mobi-1.webp";
+import ProgramMobi2 from "./Program-mobi-2.webp";
+import ProgramMobi3 from "./Program-mobi-3.webp";
+
+// images for laptop and bigger screens
+export const aboutusLapBackground = {
+  AboutUsBig1,
+  AboutUsBig2,
+  AboutUsBig3,
+};
+
+// images for tablet sized screens
+export const aboutusTabBackground = {
+  AboutUsTab1,
+  AboutUsTab2,
+  AboutUsTab3,
+  AboutUsTab4,
+  AboutUsTab5,
+  AboutUsTab6,
+};
+
+// images of mobile sized screens
+export const aboutusMobiBackground = {
+  AboutUsMobi1,
+  AboutUsMobi2,
+  AboutUsMobi3,
+};
+
+export const programLapBackground = {
+  ProgramBig1,
+  ProgramBig2,
+  ProgramBig3,
+};
+
+export const programTabBackground = {
+  ProgramTab1,
+  ProgramTab2,
+};
+
+export const programMobiBackground = {
+  ProgramMobi1,
+  ProgramMobi2,
+  ProgramMobi3,
+};
